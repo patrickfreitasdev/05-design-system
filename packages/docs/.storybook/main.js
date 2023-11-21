3087751cdca5a934ff012ddc403b88364269f6c8
Patrick de Freitas <patrickfreitas.dev@gmail.com>
2023-11-21 19:16:36 +0800

    Fix: Add subpath to StoryBook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -28,5 +28,13 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+
+  viteFinal: (config, { configType }) => {
+    if (configType == "PRODUCTION") {
+      config.base = "/05-design-system/";
+    }
+
+    return config;
+  },
 };
 export default config;
